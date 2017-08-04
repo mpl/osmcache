c3005779c941550dbe59951a7e8673bc64e720fc
mpl <mathieu.lonjaret@gmail.com>
2017-08-04 02:29:46 +0200

    first commit

---
+++ main.go
@@ -0,0 +1,172 @@
+// Copyright 2017 Mathieu Lonjaret
+
+package main
+
+import (
+	"flag"
+	"fmt"
+	"html/template"
+	"io/ioutil"
+	"log"
+	"net"
+	"net/http"
+	"os"
+	"path/filepath"
+	"sync"
+
+	"github.com/mpl/basicauth"
+	"github.com/mpl/simpletls"
+)
+
+const (
+	idstring = "http://golang.org/pkg/http/#ListenAndServe"
+)
+
+var (
+	host         = flag.String("host", "0.0.0.0:8080", "listening port and hostname")
+	help         = flag.Bool("h", false, "show this help")
+	flagUserpass = flag.String("userpass", "", "optional username:password protection")
+	flagTLS      = flag.Bool("tls", false, `For https. Uses autocert, or "key.pem" and "cert.pem" in $HOME/keys/`)
+)
+
+var (
+	rootdir, _ = os.Getwd()
+	up         *basicauth.UserPass
+	uploadTmpl *template.Template
+
+	lastServerMu sync.Mutex
+	lastServer   int
+)
+
+func usage() {
+	fmt.Fprintf(os.Stderr, "\t osmcache \n")
+	flag.PrintDefaults()
+	os.Exit(2)
+}
+
+func makeHandler(fn func(http.ResponseWriter, *http.Request, string)) http.HandlerFunc {
+	return func(w http.ResponseWriter, r *http.Request) {
+		defer func() {
+			if e, ok := recover().(error); ok {
+				http.Error(w, e.Error(), http.StatusInternalServerError)
+				return
+			}
+		}()
+		title := r.URL.Path
+		w.Header().Set("Server", idstring)
+		if isAllowed(r) {
+			fn(w, r, title)
+		} else {
+			basicauth.SendUnauthorized(w, r, "simpleHttpd")
+		}
+	}
+}
+
+func isAllowed(r *http.Request) bool {
+	if *flagUserpass == "" {
+		return true
+	}
+	return up.IsAllowed(r)
+}
+
+func pickServer() string {
+	servers := []string{"a", "b", "c"}
+	lastServerMu.Lock()
+	defer lastServerMu.Unlock()
+	lastServer++
+	if lastServer >= len(servers) {
+		lastServer = 0
+	}
+	return servers[lastServer]
+}
+
+const osmBaseURL = "https://%s.tile.openstreetmap.org"
+
+func fetchTile(url string) error {
+	println("FETCHING " + url)
+	server := pickServer()
+	resp, err := http.Get(fmt.Sprintf(osmBaseURL, server) + url)
+	if err != nil {
+		return err
+	}
+	defer resp.Body.Close()
+	tile, err := ioutil.ReadAll(resp.Body)
+	if err != nil {
+		return err
+	}
+	println("WRITING " + filepath.Join(rootdir, url))
+	fullPath := filepath.Join(rootdir, url)
+	if err := os.MkdirAll(filepath.Dir(fullPath), 0700); err != nil {
+		return err
+	}
+	if err := ioutil.WriteFile(fullPath, tile, 0700); err != nil {
+		return err
+	}
+	return nil
+}
+
+func serveTile(w http.ResponseWriter, r *http.Request, url string) {
+	fullPath := filepath.Join(rootdir, url)
+	if _, err := os.Stat(fullPath); err != nil {
+		if !os.IsNotExist(err) {
+			http.Error(w, "nope", 500)
+			log.Print(err)
+			return
+		}
+		if err := fetchTile(url); err != nil {
+			http.Error(w, "nope", 500)
+			log.Print(err)
+			return
+		}
+	}
+	println("SERVING " + url)
+	http.ServeFile(w, r, fullPath)
+}
+
+func initUserPass() {
+	if *flagUserpass == "" {
+		return
+	}
+	var err error
+	up, err = basicauth.New(*flagUserpass)
+	if err != nil {
+		log.Fatal(err)
+	}
+}
+
+func main() {
+	flag.Usage = usage
+	flag.Parse()
+	if *help {
+		usage()
+	}
+
+	nargs := flag.NArg()
+	if nargs > 0 {
+		usage()
+	}
+
+	initUserPass()
+
+	*simpletls.FlagAutocert = false
+
+	if !*flagTLS && *simpletls.FlagAutocert {
+		*flagTLS = true
+	}
+
+	var err error
+	var listener net.Listener
+	if *flagTLS {
+		listener, err = simpletls.Listen(*host)
+	} else {
+		listener, err = net.Listen("tcp", *host)
+	}
+	if err != nil {
+		log.Fatalf("Failed to listen on %s: %v", *host, err)
+	}
+
+	http.Handle("/", makeHandler(serveTile))
+	if err = http.Serve(listener, nil); err != nil {
+		log.Fatalf("Error in http server: %v\n", err)
+	}
+}
